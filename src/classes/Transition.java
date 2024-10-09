f002ba8f2c7cea1ea5d38d901cc292b4a7315a71
GILLARD Antonino <antonino.gillard@imt-atlantique.net>
2024-10-09 14:42:45 +0200

    Added base methods for transition (tests & comments to be added)

--- src/classes/Transition.java
+++ src/classes/Transition.java
@@ -1,10 +1,49 @@
 package classes;
 
-public class Transition {
+import java.util.ArrayList;
 
+import arcs.ArcPT;
+import arcs.ArcTP;
+
+public class Transition {
+	private ArrayList<ArcTP> arcsTP;
+	private ArrayList<ArcPT> arcsPT;
 	
 	public Transition() {
 		// TODO Auto-generated constructor stub
 	}
+	
+	public void addArcTP(ArcTP a) {
+		if (a != null) {
+			this.arcsTP.add(a);
+		}
+	}
+	
+	public void addArcPT(ArcPT a) {
+		if (a != null) {
+			this.arcsPT.add(a);
+		}
+	}
+	
+	public ArrayList<ArcTP> getArcsTP() {
+		return this.arcsTP;
+	}
+	
+	public ArrayList<ArcPT> getArcsPT() {
+		return this.arcsPT;
+	}
+	
+	public void removeArcTP(ArcTP a) {
+		if (a != null) {
+			this.arcsTP.remove(a);
+		}
+	}
+	
+	public void removeArcPT(ArcPT a) {
+		if (a != null) {
+			this.arcsPT.remove(a);
+		}
+	}
+	
 
 }
