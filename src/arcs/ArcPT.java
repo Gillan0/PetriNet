6de8deb9ba9690db56f6cca86e31e201ac70e9c2
W0lfgunbl00d <elakrabasouissi@gmail.com>
2024-10-09 14:57:10 +0200

    Added Arcs

--- src/arcs/ArcPT.java
+++ src/arcs/ArcPT.java
@@ -1,11 +1,22 @@
 package arcs;
 
 import classes.Arc;
+import classes.Place;
+import classes.Transition;
 
 public class ArcPT extends Arc {
+	
 
-	public ArcPT() {
-		// TODO Auto-generated constructor stub
+	public ArcPT(int weight, Place place, Transition transition) {
+		super(weight, place, transition);
+	}
+	
+	public boolean isFireable() {
+		return false;
+	}
+	
+	public void removeTokens() {
+		return;
 	}
 
 }
--- src/arcs/ArcZero.java
+++ src/arcs/ArcZero.java
@@ -1,11 +1,21 @@
 package arcs;
 
 import classes.Arc;
+import classes.Place;
+import classes.Transition;
 
 public class ArcZero extends Arc {
 
-	public ArcZero() {
-		// TODO Auto-generated constructor stub
+	public ArcZero(int weight, Place place, Transition transition) {
+		super(weight, place, transition);
+	}
+	
+	public boolean isFireable() {
+		return false;
+	}
+	
+	public boolean isActive() {
+		return false;
 	}
 
 }
